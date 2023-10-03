62fb7942c785edcef7d7e02f8bf425f29497b0a8
RobanSeyidoglu <robanseyidoglu@gmail.com>
2023-10-03 17:16:50 +0300

    selecet all elements that will be needed

---
+++ main.js
@@ -0,0 +1,5 @@
+// Select all <audio> elements on the page
+let audioKey = document.querySelectorAll("audio");
+
+// Select all elements with the class "key" on the page
+let keyboard = document.querySelectorAll(".key");
